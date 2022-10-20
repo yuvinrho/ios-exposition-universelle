7954cda36f6ab1580d58e19f4c44d3401d861bd7
yuvinrho <yvrho@naver.com>
2022-10-20 09:59:55 +0900

    feat: ItemInfo화면 스택뷰 삭제, 스크롤뷰로 사용

--- Expo1900/Expo1900/Controllers/ItemInfoViewController.swift
+++ Expo1900/Expo1900/Controllers/ItemInfoViewController.swift
@@ -8,32 +8,25 @@
 import UIKit
 
 final class ItemInfoViewController: UIViewController {
-    @IBOutlet private weak var stackView: UIStackView!
+    @IBOutlet private weak var itemImage: UIImageView!
+    @IBOutlet private weak var itemDescriptionLabel: UILabel!
     var item: Item?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         configureNavigation()
-        configureStackView()
+        configureView()
     }
     
     private func configureNavigation() {
         self.navigationItem.title = item?.name
     }
     
-    private func configureStackView() {
-        let itemImageView = UIImageView(image: UIImage(named: item?.imageName ?? "flag"))
-        let itemDescriptionLabel: UILabel = {
-            let label = UILabel()
-            label.text = item?.desc
-            label.numberOfLines = 0
-            label.font = UIFont.preferredFont(forTextStyle: .body)
-            
-            return label
-        }()
-        
-        stackView.addArrangedSubview(itemImageView)
-        stackView.addArrangedSubview(itemDescriptionLabel)
+    private func configureView() {
+        itemImage.image = UIImage(named: item?.imageName ?? "flag")
+        itemImage.contentMode = .scaleAspectFit
+        itemImage.clipsToBounds = true
+        itemDescriptionLabel.text = item?.desc
     }
 }
